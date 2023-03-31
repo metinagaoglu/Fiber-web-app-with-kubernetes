fa7e934ede3d0c1962116410fdb7e0a234a5d7c6
Metin Ağaoğlu <metnagaoglu@gmail.com>
2023-03-31 23:46:39 +0300

    Implement first version of auth microservice.

---
+++ src/auth-service/cmd/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net"
+
+    "auth-service/pkg/config"
+    "auth-service/pkg/db"
+    "auth-service/pkg/pb"
+    "auth-service/pkg/services"
+    "auth-service/pkg/utils"
+    "google.golang.org/grpc"
+)
+
+func main() {
+    c, err := config.LoadConfig()
+
+    if err != nil {
+        log.Fatalln("Failed at config", err)
+    }
+    h := db.Init(c.DBUrl)
+
+    jwt := utils.JwtWrapper{
+        SecretKey:       c.JWTSecretKey,
+        Issuer:          "go-grpc-auth-svc",
+        ExpirationHours: 24 * 365,
+    }
+
+    lis, err := net.Listen("tcp", c.Port)
+
+    if err != nil {
+        log.Fatalln("Failed to listing:", err)
+    }
+
+    fmt.Println("Auth Svc on", c.Port)
+
+    s := services.Server{
+        H:   h,
+        Jwt: jwt,
+    }
+
+    grpcServer := grpc.NewServer()
+
+    pb.RegisterAuthServiceServer(grpcServer, &s)
+
+    if err := grpcServer.Serve(lis); err != nil {
+        log.Fatalln("Failed to serve:", err)
+    }
+} 
